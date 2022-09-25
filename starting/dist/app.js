2e9e31aff80313f856bc4ba799b5d2d00b56774a
oshcherbak <oshcherbak@griddynamics.com>
2022-09-25 18:55:46 +0300

    lesson8

--- starting/dist/app.js
+++ starting/dist/app.js
@@ -1,7 +1,78 @@
-"use strict";
-function add(n1, n2) {
-    return n1 + n2;
-}
-const num1 = 5;
-const num2 = 10;
-console.log(add(num1, num2));
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Department = /** @class */ (function () {
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+        this.id = id;
+        this.name = name;
+    }
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    return Department;
+}());
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, "IT") || this;
+        _this.admins = admins;
+        _this.admins = admins;
+        return _this;
+    }
+    ITDepartment.prototype.describe = function () {
+        console.log("Department(".concat(this.id, "): IT"));
+    };
+    return ITDepartment;
+}(Department));
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id) {
+        return _super.call(this, id, "Accounting") || this;
+    }
+    AccountingDepartment.prototype.describe = function () {
+        console.log("Department(".concat(this.id, "): Accounting"));
+    };
+    AccountingDepartment.getInstance = function () {
+        if (AccountingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new AccountingDepartment("d2");
+        return this.instance;
+    };
+    return AccountingDepartment;
+}(Department));
+var it = new ITDepartment("v1", ["Oleh", "Decor"]);
+it.addEmployee("Ann");
+it.addEmployee("Dil");
+it.printEmployeeInformation();
+it.describe();
+var accountingDepartment = AccountingDepartment.getInstance();
+var accountingDepartment2 = AccountingDepartment.getInstance();
+console.log(accountingDepartment, accountingDepartment2);
+var user1;
+user1 = {
+    name: "Ann",
+    age: 21
+};
+console.log(user1);
+var add = function (n1, n2) { return n1 + n2; };
+console.log(add(1, 2));
--- starting/src/app.ts
+++ starting/src/app.ts
@@ -1,8 +1,87 @@
-function add(n1: number, n2: number) {
-    return n1 + n2
+abstract class Department {
+  private employees: string[] = [];
+
+  constructor(protected readonly id: string, private name: string) {
+    this.id = id;
+    this.name = name;
+  }
+
+  abstract describe(): void;
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
-const num1 = 5;
-const num2 = 10;
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
 
-console.log(add(num1, num2));
+  describe() {
+    console.log(`Department(${this.id}): IT`);
+  }
+}
+
+class AccountingDepartment extends Department {
+  private static instance: AccountingDepartment;
+  private constructor(id: string) {
+    super(id, "Accounting");
+  }
+
+  describe(): void {
+    console.log(`Department(${this.id}): Accounting`);
+  }
+
+  static getInstance() {
+    if (AccountingDepartment.instance) {
+      return this.instance;
+    }
+
+    this.instance = new AccountingDepartment("d2");
+    return this.instance;
+  }
+}
+
+const it = new ITDepartment("v1", ["Oleh", "Decor"]);
+
+it.addEmployee("Ann");
+it.addEmployee("Dil");
+
+it.printEmployeeInformation();
+
+it.describe();
+
+const accountingDepartment = AccountingDepartment.getInstance();
+const accountingDepartment2 = AccountingDepartment.getInstance();
+
+console.log(accountingDepartment, accountingDepartment2);
+
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+let user1: Person;
+
+user1 = {
+  name: "Ann",
+  age: 21,
+};
+
+console.log(user1);
+
+interface addFn {
+  (a: number, b: number): number;
+}
+
+const add: addFn = (n1, n2) => n1 + n2;
+
+console.log(add(1, 2));
